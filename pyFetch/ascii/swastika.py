25fc3aadfd257164a60fe2cbc0291c680c53f5f6
mavanmanen <mitchell_oo@live.nl>
2013-04-19 10:31:31 +0200

    swastika tiem

---
+++ pyFetch/ascii/swastika.py
@@ -0,0 +1,18 @@
+from colorama import Fore, Back, Style
+
+ascii_art = [
+                "                    ",
+    Fore.RED + "卐        卐卐卐卐卐卐 ",
+    Fore.RED + "卐        卐         ",
+    Fore.RED + "卐        卐         ",
+    Fore.RED + "卐        卐         ",
+    Fore.RED + "卐        卐         ",
+    Fore.RED + "卐        卐         ",
+    Fore.RED + "卐卐卐卐卐卐卐卐卐卐卐卐",
+    Fore.RED + "          卐       卐",
+    Fore.RED + "          卐       卐",
+    Fore.RED + "          卐       卐",
+    Fore.RED + "          卐       卐",
+    Fore.RED + "          卐       卐",
+    Fore.RED + "卐卐卐卐卐卐卐       卐",
+]
