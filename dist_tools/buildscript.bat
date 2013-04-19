8765f6d664a9ca94d5b0d43bb16dffed197609b7
Zarek Jenkinson <zarek.p.jenkinson@gmail.com>
2013-04-19 18:06:04 +1200

    lol

--- dist_tools/buildscript.bat
+++ dist_tools/buildscript.bat
@@ -3,4 +3,4 @@ echo Commit: %GITCOMMIT% > buildinfo
 echo Build: %BUILD_TAG% >> buildinfo
 echo Build URL: %BUILD_URL% >> buildinfo
 
-c:\python27\python.exe setup.py py2exe
+c:\python27\python.exe C:\Python27\Lib\site-packages\pyinstaller\pyinstaller.py -F pyFetch.py
