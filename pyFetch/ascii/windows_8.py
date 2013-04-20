64535b362107649eb268deebbc3f723ba157fc9e
Zarek <zarek.p.jenkinson@gmail.com>
2013-04-21 11:47:53 +1200

    Revert "centered win8 logo"

--- pyFetch/ascii/windows_8.py
+++ pyFetch/ascii/windows_8.py
@@ -2,7 +2,6 @@ from colorama import Fore, Back, Style
 
 ascii_art = [
                 "                    ",
-                "                    ",
     Fore.CYAN + "         .::::::::: ",
     Fore.CYAN + " .:::::: :::::::::: ",
     Fore.CYAN + " ::::::: :::::::::: ",
