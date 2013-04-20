47373f1c1b18aa5de8f1ce0f4e8cd4065f8d1ca4
Zarek <zarek.p.jenkinson@gmail.com>
2013-04-21 04:40:13 +1200

    cleaned up buildscript (one line, hah)

--- dist_tools/buildscript.bat
+++ dist_tools/buildscript.bat
@@ -1,6 +1,1 @@
-if exist ".git/refs/heads/master" ( set /p GITCOMMIT=<".git/refs/heads/master" ) else ( set GITCOMMIT=unknown )
-echo Commit: %GITCOMMIT% > buildinfo
-echo Build: %BUILD_TAG% >> buildinfo
-echo Build URL: %BUILD_URL% >> buildinfo
-
 c:\python27\python.exe C:\Python27\Lib\site-packages\pyinstaller\pyinstaller.py -F pyFetch.py
